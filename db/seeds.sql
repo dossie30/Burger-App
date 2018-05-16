9c900bf45edb67f7d463529cf9426a0c49864cd0
Dawn Tossie <dawn.tossie@gmail.com>
2018-05-16 00:04:22 -0400

    directory created

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Junior Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
