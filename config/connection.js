8bd78913198317899ea7183b6b8787cb4da8d071
Dawn Tossie <dawn.tossie@gmail.com>
2018-05-19 12:50:36 -0400

    added JAWS_DB info to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "orchard4227",
-  database: "burgers_db"
-});
+var connection;
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "orchard4227",
+    database: "burgers_db"
+  })
+};
 
 // Export connection for our ORM to use.
 module.exports = connection;
